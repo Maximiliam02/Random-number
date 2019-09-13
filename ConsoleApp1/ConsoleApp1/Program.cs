d1b0e6f119fb6b130bb07a6763bbde60453c5558
Elev <Elev@TEB127033>
2019-09-13 08:54:19 +0200

    Jag har gjort ett programm som låter dig gissa ett nummer dom programmet har valt mellan 1-100 sedan säger det mindre eller högre beroende på vad du gissat tills du gissar rätt.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Programmering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random Talet = new Random();
+            int talet = Talet.Next(100);
+
+            Boolean vunnit = false;
+
+            while (vunnit == false)
+            {
+                Console.WriteLine("Gissa ett tal emellan 1-100");
+                string Gissning = Console.ReadLine();
+                int.TryParse(Gissning, out int gissning);
+
+
+                if (gissning == talet)
+                {
+                    vunnit = true;
+                    Console.WriteLine("Rätt");
+                }
+
+                else if (gissning > talet)
+                {
+                    Console.WriteLine("Mindre!");
+
+
+                }
+
+                else if (gissning < talet)
+                {
+                    Console.WriteLine("Större!");
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
